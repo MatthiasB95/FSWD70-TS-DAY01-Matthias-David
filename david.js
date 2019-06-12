fe88769c937995d76e0c1dc1baf2e9cce4769740
David Duma <davidumapag@gmail.com>
2019-06-12 13:17:55 +0200

    david

---
+++ david.js
@@ -0,0 +1,10 @@
+/* Basic
+
+var table : Array<number> = [1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < table.length; i++){
+    for (var x = 1; x <= 10; x++){
+        let y = x * table[i];
+        document.write(x ' X ' table[i] ' = ' y '<br>');
+    }
+}*/
